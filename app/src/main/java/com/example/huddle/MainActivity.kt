f3eae8c3c7112f2eaaedb4cd0282d944daf94535
Aryan Srivastava <aryansrivastava9059@gmail.com>
2024-12-18 18:17:18 +0530

    Added XML for Login & Sign Up Screens

--- app/src/main/java/com/example/huddle/MainActivity.kt
+++ app/src/main/java/com/example/huddle/MainActivity.kt
@@ -3,8 +3,6 @@ package com.example.huddle
 import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.splashscreen.SplashScreen
-import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.example.huddle.components.login.LoginActivity
