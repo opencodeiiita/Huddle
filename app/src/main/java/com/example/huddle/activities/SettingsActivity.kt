e41f50f02cbf2a2deb10a86acbcecd56c7d025c4
PriyamJChakrabarty <144861619+PriyamJChakrabarty@users.noreply.github.com>
2024-12-28 18:11:27 +0530

    Added the Back Function
    Added the Back Function

--- app/src/main/java/com/example/huddle/activities/SettingsActivity.kt
+++ app/src/main/java/com/example/huddle/activities/SettingsActivity.kt
@@ -4,11 +4,17 @@ package com.example.huddle.activities
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.example.huddle.R
+import com.google.android.material.card.MaterialCardView
 
 class SettingsActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_settings)
+
+        findViewById<MaterialCardView>(R.id.settings_back_button).setOnClickListener {
+            // Finish the current activity to go back to the previous one (ProfileFragment or another activity)
+            finish()
+        }
 
         // Add your settings-related logic here
     }
