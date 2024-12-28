8f2043a77c5786fa3c63af483fc2ee3fba260121
PriyamJChakrabarty <144861619+PriyamJChakrabarty@users.noreply.github.com>
2024-12-28 17:49:57 +0530

    Added the Settings
    Added the settings

---
+++ app/src/main/java/com/example/huddle/activities/SettingsActivity.kt
@@ -0,0 +1,15 @@
+
+package com.example.huddle.activities
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.huddle.R
+
+class SettingsActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_settings)
+
+        // Add your settings-related logic here
+    }
+}
